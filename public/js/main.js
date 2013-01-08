bae34dd340adb8fdefa9b92c9115fc68d9517e84
Alexey Frank <frank@MacBook-Air-Alexey.local>
2013-01-08 23:13:53 +0400

    new assets

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,80 @@
 $(document).ready(function(){
+	initParallax();
 	initMainMenu();
 	initEvents();
+	initPopUp();
+	initPolozhenie();
+	initLaureate();
 });
+
+initParallax = function() {
+	$('.parallax1,.parallax2,.parallax3').parallax({
+		mouseport: jQuery('body'),
+		yparallax: false,
+		mouseResponse: false
+	});
+};
+
+initLaureate = function() {
+	$('.b-wrap-laureate-item').width($('.b-laureate-item').length*978);
+	var scroll = $('.scroll-pane').jScrollPane({
+		showArrows: false,
+		horizontalDragMinWidth: 23,
+		horizontalDragMaxWidth: 23,
+		animateScroll: true,
+		clickOnTrack: true
+	}).data('jsp');
+
+	$('.prev-laureate').bind('click', function(){
+		scroll.scrollByX(-100);
+		return false;
+	});
+	$('.next-laureate').bind('click', function(){
+		scroll.scrollByX(100);
+		return false;
+	});
+
+	$('.b-laureate img').mouseenter(function(){
+		$(this)
+			.next()
+			.show()
+			.mouseleave(function(){
+				$(this).hide();
+			});
+	});
+};
+
+initPopUp = function() {
+	var open_pop_up = $('.open-pop-up');
+
+	if(!open_pop_up.length) {
+		return false;
+	}
+
+	var shadow = $('.shadow');
+	var pop_up = $('.pop-up');
+
+	open_pop_up.click(function(){
+		shadow.show();
+		var offset = $(this).offset();
+		pop_up
+			.show()
+			.css({
+				'left' : offset.left,
+				'top'  : offset.top - pop_up.outerHeight() + 10
+			});
+		return false;
+	});
+
+	$('.pop-up .pop-up-close,.shadow').click(function(){
+		popUpHide();
+	});
+
+	popUpHide = function() {
+		shadow.hide();
+		pop_up.hide();
+	}
+};
 
 initMainMenu = function() {
 	var sub_menu = $('.wrap-sub-menu');
@@ -38,13 +111,44 @@ initMainMenu = function() {
 };
 
 initEvents = function() {
-	$(".b-events").carouFredSel({
+	events = $('.b-events');
+
+	if(!events.length) {
+		return false;
+	}
+
+	events.carouFredSel({
 		auto: false,
 		next : {
 			button: $('.b-event2-arrow-right')
 		},
 		prev : {
 			button: $('.b-event3-arrow-left')
+		}
+	});
+};
+
+initPolozhenie = function() {
+	polozhenie = $('.polozhenie');
+
+	if(!polozhenie.length) {
+		return false;
+	}
+
+	$('.next-top,.next-bottom,.prev-top,.prev-bottom').click(function(){
+		$('html, body').animate({scrollTop:0},'slow');
+	});
+
+	polozhenie.carouFredSel({
+		auto: false,
+		scroll : {
+			fx: 'crossfade'
+		},
+		next : {
+			button: $('.next-top,.next-bottom')
+		},
+		prev : {
+			button: $('.prev-top,.prev-bottom')
 		}
 	});
 };
