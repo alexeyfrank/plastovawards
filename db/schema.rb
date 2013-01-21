ecb2dd98b1da2822170becd05805ac02fb0a156e
Alexey Frank <frank@MacBook-Air-Alexey.local>
2013-01-21 11:46:11 +0400

    fix menu

--- db/schema.rb
+++ db/schema.rb
@@ -14,39 +14,39 @@
 ActiveRecord::Schema.define(:version => 20130114191520) do
 
   create_table "bid_states", :force => true do |t|
-    t.string    "name"
-    t.timestamp "created_at", :null => false
-    t.timestamp "updated_at", :null => false
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "ckeditor_assets", :force => true do |t|
-    t.string    "data_file_name",                  :null => false
-    t.string    "data_content_type"
-    t.integer   "data_file_size"
-    t.integer   "assetable_id"
-    t.string    "assetable_type",    :limit => 30
-    t.string    "type",              :limit => 30
-    t.integer   "width"
-    t.integer   "height"
-    t.timestamp "created_at",                      :null => false
-    t.timestamp "updated_at",                      :null => false
+    t.string   "data_file_name",                  :null => false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    :limit => 30
+    t.string   "type",              :limit => 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
   end
 
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_ckeditor_assetable_type"
 
   create_table "competition_states", :force => true do |t|
-    t.string    "name"
-    t.timestamp "created_at", :null => false
-    t.timestamp "updated_at", :null => false
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "feedbacks", :force => true do |t|
-    t.string    "email"
-    t.string    "phone"
-    t.text      "text"
-    t.timestamp "created_at", :null => false
-    t.timestamp "updated_at", :null => false
+    t.string   "email"
+    t.string   "phone"
+    t.text     "text"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "member_pictures", :force => true do |t|
@@ -67,40 +67,40 @@ ActiveRecord::Schema.define(:version => 20130114191520) do
   end
 
   create_table "members", :force => true do |t|
-    t.string    "first_name"
-    t.string    "last_name"
-    t.string    "birth_year"
-    t.string    "country"
-    t.string    "city"
-    t.string    "phone"
-    t.string    "email"
-    t.timestamp "created_at",    :null => false
-    t.timestamp "updated_at",    :null => false
-    t.integer   "nomination_id"
-    t.integer   "year"
-    t.string    "state"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "birth_year"
+    t.string   "country"
+    t.string   "city"
+    t.string   "phone"
+    t.string   "email"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.integer  "nomination_id"
+    t.integer  "year"
+    t.string   "state"
   end
 
   create_table "menu_item_translations", :force => true do |t|
-    t.integer   "menu_item_id"
-    t.string    "locale"
-    t.string    "name"
-    t.timestamp "created_at",   :null => false
-    t.timestamp "updated_at",   :null => false
+    t.integer  "menu_item_id"
+    t.string   "locale"
+    t.string   "name"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   add_index "menu_item_translations", ["locale"], :name => "index_menu_item_translations_on_locale"
   add_index "menu_item_translations", ["menu_item_id"], :name => "index_menu_item_translations_on_menu_item_id"
 
   create_table "menu_items", :force => true do |t|
-    t.string    "name"
-    t.string    "slug"
-    t.string    "ancestry"
-    t.string    "klass"
-    t.integer   "menu_id"
-    t.timestamp "created_at", :null => false
-    t.timestamp "updated_at", :null => false
-    t.integer   "position"
+    t.string   "name"
+    t.string   "slug"
+    t.string   "ancestry"
+    t.string   "klass"
+    t.integer  "menu_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "position"
   end
 
   add_index "menu_items", ["ancestry"], :name => "index_menu_items_on_ancestry"
@@ -108,115 +108,110 @@ ActiveRecord::Schema.define(:version => 20130114191520) do
   add_index "menu_items", ["position"], :name => "index_menu_items_on_position"
 
   create_table "menus", :force => true do |t|
-    t.string    "name"
-    t.string    "description"
-    t.timestamp "created_at",  :null => false
-    t.timestamp "updated_at",  :null => false
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   add_index "menus", ["name"], :name => "index_menus_on_name"
 
   create_table "news", :force => true do |t|
-    t.text      "title"
-    t.text      "content"
-    t.integer   "author_id"
-    t.timestamp "created_at",  :null => false
-    t.timestamp "updated_at",  :null => false
-    t.string    "state"
-    t.integer   "category_id"
+    t.text     "title"
+    t.text     "content"
+    t.integer  "author_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "state"
+    t.integer  "category_id"
   end
 
   create_table "news_categories", :force => true do |t|
-    t.string    "title"
-    t.string    "state"
-    t.timestamp "created_at", :null => false
-    t.timestamp "updated_at", :null => false
+    t.string   "title"
+    t.string   "state"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "news_translations", :force => true do |t|
-    t.integer   "news_id"
-    t.string    "locale"
-    t.string    "title"
-    t.text      "content"
-    t.timestamp "created_at", :null => false
-    t.timestamp "updated_at", :null => false
+    t.integer  "news_id"
+    t.string   "locale"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "news_translations", ["locale"], :name => "index_news_translations_on_locale"
   add_index "news_translations", ["news_id"], :name => "index_news_translations_on_news_id"
 
   create_table "nomination_translations", :force => true do |t|
-    t.integer   "nomination_id"
-    t.string    "locale"
-    t.string    "name"
-    t.timestamp "created_at",    :null => false
-    t.timestamp "updated_at",    :null => false
+    t.integer  "nomination_id"
+    t.string   "locale"
+    t.string   "name"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
   add_index "nomination_translations", ["locale"], :name => "index_nomination_translations_on_locale"
   add_index "nomination_translations", ["nomination_id"], :name => "index_nomination_translations_on_nomination_id"
 
   create_table "nominations", :force => true do |t|
-    t.string    "name"
-    t.timestamp "created_at", :null => false
-    t.timestamp "updated_at", :null => false
-    t.string    "state"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "state"
   end
 
   create_table "page_translations", :force => true do |t|
-    t.integer   "page_id"
-    t.string    "locale"
-    t.string    "title"
-    t.text      "content"
-    t.timestamp "created_at", :null => false
-    t.timestamp "updated_at", :null => false
+    t.integer  "page_id"
+    t.string   "locale"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "page_translations", ["locale"], :name => "index_page_translations_on_locale"
   add_index "page_translations", ["page_id"], :name => "index_page_translations_on_page_id"
 
   create_table "pages", :force => true do |t|
-    t.integer   "author_id"
-    t.string    "title"
-    t.text      "content"
-    t.string    "state"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.string    "slug"
-    t.boolean   "is_home_page"
+    t.integer  "author_id"
+    t.string   "title"
+    t.text     "content"
+    t.string   "state"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+    t.string   "slug"
+    t.boolean  "is_home_page"
   end
 
-  create_table "pictures", :force => true do |t|
-    t.text      "description"
-    t.date      "create_year"
-    t.string    "size"
-    t.string    "technique"
-    t.integer   "member_id"
-    t.timestamp "created_at",  :null => false
-    t.timestamp "updated_at",  :null => false
-  end
+  add_index "pages", ["author_id"], :name => "index_pages_on_author_id"
+  add_index "pages", ["slug"], :name => "index_pages_on_slug"
+  add_index "pages", ["state"], :name => "index_pages_on_state"
+  add_index "pages", ["title"], :name => "index_pages_on_title"
 
   create_table "settings", :force => true do |t|
-    t.string    "var",                       :null => false
-    t.text      "value"
-    t.integer   "target_id"
-    t.string    "target_type", :limit => 30
-    t.timestamp "created_at",                :null => false
-    t.timestamp "updated_at",                :null => false
+    t.string   "var",                       :null => false
+    t.text     "value"
+    t.integer  "target_id"
+    t.string   "target_type", :limit => 30
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   add_index "settings", ["target_type", "target_id", "var"], :name => "index_settings_on_target_type_and_target_id_and_var", :unique => true
 
   create_table "subscribers", :force => true do |t|
-    t.string    "email"
-    t.string    "fio"
-    t.string    "phone"
-    t.boolean   "processing_personal_data"
-    t.boolean   "notify_about_center_events"
-    t.timestamp "created_at",                 :null => false
-    t.timestamp "updated_at",                 :null => false
-    t.boolean   "in_timepad"
-    t.boolean   "in_google_drive"
+    t.string   "email"
+    t.string   "fio"
+    t.string   "phone"
+    t.boolean  "processing_personal_data"
+    t.boolean  "notify_about_center_events"
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.boolean  "in_timepad"
+    t.boolean  "in_google_drive"
   end
 
   add_index "subscribers", ["email"], :name => "index_subscribers_on_email"
@@ -224,13 +219,13 @@ ActiveRecord::Schema.define(:version => 20130114191520) do
   add_index "subscribers", ["processing_personal_data"], :name => "index_subscribers_on_processing_personal_data"
 
   create_table "taggings", :force => true do |t|
-    t.integer   "tag_id"
-    t.integer   "taggable_id"
-    t.string    "taggable_type"
-    t.integer   "tagger_id"
-    t.string    "tagger_type"
-    t.string    "context",       :limit => 128
-    t.timestamp "created_at"
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       :limit => 128
+    t.datetime "created_at"
   end
 
   add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
@@ -241,12 +236,12 @@ ActiveRecord::Schema.define(:version => 20130114191520) do
   end
 
   create_table "users", :force => true do |t|
-    t.string    "email"
-    t.string    "password_digest"
-    t.string    "first_name"
-    t.string    "last_name"
-    t.timestamp "created_at",      :null => false
-    t.timestamp "updated_at",      :null => false
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
